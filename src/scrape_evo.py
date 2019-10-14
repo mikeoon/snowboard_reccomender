23a59d14680afc15d2795e13d086846f7ae3abd0
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-10-14 16:11:40 -0700

    add clean scrape features and start of sql database

--- src/scrape_evo.py
+++ src/scrape_evo.py
@@ -1,8 +1,4 @@
-import copy
-import pandas as pd
-from pymongo import MongoClient
-import pprint
-
+import psycopg2
 
 # Requests sends and recieves HTTP requests.
 import requests
@@ -11,18 +7,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def scrape_product(evo_url):
+def _scrape_product(evo_url):
 	#evo_url = 'https://www.evo.com/snowboards/capita-spring-break-slush-slasher-snowboard#image=161992/642522/capita-spring-break-slush-slasher-snowboard-2020-.jpg'
 	#vo_url = 'https://www.evo.com/snowboards/jones-mountain-twin-snowboard#image=161602/640791/clone.jpg'
 	r = requests.get(evo_url)
-
+	print(r)
 
 	soup = BeautifulSoup(r.content, "lxml")
 
+	new_snwb = dict()
+	new_snwb_size = dict()
+
 	# find name of product
 	product_name = soup.find(id='buy-grid').find('h1', {'class': 'pdp-header-title'})
-	print(product_name.string)
-
+	new_snwb['name'] = product_name.string.strip()
+	new_snwb['id'] = new_snwb_size['id'] = soup.find('span', {'class' : 'pdp-header-util-sku'}).string[4:].strip()
+	
+	# add price some other way
+	#new_snwb['price'] = soup.find('span', {'class':'pdp-price-regular pdp-price-display no-wrap'})
 
 	product_detail = soup.find('div', {'class':'mobile-accordion-group'})
 
@@ -32,9 +34,13 @@ def scrape_product(evo_url):
 
 	for i in product_rows:
 		#product detail name
-		print(i.find('h5').string)
+		p_detail = i.find('h5').string.lower().replace(' ', '_')
+		if p_detail == 'flex':
+			p_data = i.find('p').find('em').string[:2].strip()
 		# product detail info
-		print(i.find('p').find('em').string)
+		else:
+			p_data = i.find('p').find('em').string
+		new_snwb[p_detail] = p_data
 
 	# Gets from the Specs tabel
 	product_specs = product_detail.find('ul', {'class':'pdp-spec-list'})
@@ -43,22 +49,20 @@ def scrape_product(evo_url):
 
 	for i in product_specs_rows:
 		# spec name
-		print(i.find('span', {'class': 'pdp-spec-list-title'}).find('strong').string)
+		p_detail = i.find('span', {'class': 'pdp-spec-list-title'}).find('strong').string[:-1].lower().replace(' ', '_').replace('/', '_')
 		# spec info
-		print(i.find('span',{'class':'pdp-spec-list-description'}).string)
+		p_data = i.find('span',{'class':'pdp-spec-list-description'}).string
+		new_snwb[p_detail] = p_data
 
-
+	# Gets the different size info
 	product_mments = product_detail.find('table', {'class':'spec-table table'})
 	product_sizes = product_mments.find('thead')
-	size_title = product_sizes.find('th').string
 
-	# Gets the size title
-	print(size_title)
 
 	for i in product_sizes.find_all('td'):
 		# The different sizes, each index is corresponds to the values below at the same index
-		print(i.string)
-
+		#new_snwb_size[product_name.string.strip() + i.string]
+		print()
 	product_mments_details = product_mments.find('tbody').find_all('tr')
 	attributes = [tr.find_all('td') for tr in product_mments_details]
 
@@ -68,10 +72,17 @@ def scrape_product(evo_url):
 		print(i.find('th').string)
 		# measurements indexed by sizes above, same order
 		print([a.string for a in j])
+	print(new_snwb)
 
 
 
-def scrape_snwb_urls():
+def _create_postgres():
+	conn = psycopg2.connect(dbname='postgres', user='postgres', host='localhost')
+	cur = conn.cursor()
+
+
+
+def _scrape_snwb_urls():
 	pages = ['', '/p_2', '/p_3']
 	evo_snwb_urls = []
 	for p in pages:
@@ -87,6 +98,6 @@ def scrape_snwb_urls():
 	return evo_snwb_urls
 
 
-
+_scrape_product('https://www.evo.com/snowboards/capita-spring-break-slush-slasher-snowboard#image=161992/642522/capita-spring-break-slush-slasher-snowboard-2020-.jpg')
 
 
---
+++ src/snwb_tables.sql
@@ -0,0 +1,27 @@
+
+
+
+
+
+CREATE TABLE snowboards(
+	name VARCHAR,
+	id INTEGER,
+	rocker_type VARCHAR,
+	flex ,
+	shape,
+	core,
+	laminates,
+	sidewalls,
+	base,
+	edges,
+	topsheet,
+	graphics,
+	additional_features,
+	binding_compatibility,
+	terrain,
+	ability_level,
+	flex_rating,
+	binding_mount_pattern,
+	core/laminates,
+	warranty,
+		);
